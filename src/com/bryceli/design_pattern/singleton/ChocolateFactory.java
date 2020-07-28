1431de53648b0e3341eeb353789101dfb103f2b8
bryce <741866303@qq.com>
2020-07-28 10:24:11 +0800

    【单例模式】优化三：双重检查加锁法，真正地只使用一次同步方法块

--- src/com/bryceli/design_pattern/singleton/ChocolateFactory.java
+++ src/com/bryceli/design_pattern/singleton/ChocolateFactory.java
@@ -8,9 +8,10 @@ package com.bryceli.design_pattern.singleton;
  */
 public class ChocolateFactory {
 
-    //优化二：急切创建对象，类加载时就直接创建对象
-    //缺点，如果运行时没有使用该类对象，则造成资源浪费
-    private static ChocolateFactory uniqueInstance = new ChocolateFactory();
+    //优化三：双重检查加锁法
+    //volatile变量，用来确保将变量的更新操作通知到其他线程,使所有的线程拿到的对象都是一样的
+    //volatile变量是一种比sychronized关键字更轻量级的同步机制,在访问volatile变量时不会执行加锁操作，因此也就不会使执行线程阻塞
+    private volatile static ChocolateFactory uniqueInstance = null;
 
     private ChocolateFactory() {
         empty = true;
@@ -18,6 +19,18 @@ public class ChocolateFactory {
     }
 
     public static synchronized ChocolateFactory getInstance() {
+        //优点：只会发生一次锁竞争，同步区域只会使用一次，其他的调用都不会进入同步代码块
+        //第一次检查
+        if (uniqueInstance == null) {
+            //加同步锁
+            synchronized (ChocolateFactory.class){
+                //第二次检查
+                if (uniqueInstance == null){
+
+                    uniqueInstance = new ChocolateFactory();
+                }
+            }
+        }
         return uniqueInstance;
     }
 
